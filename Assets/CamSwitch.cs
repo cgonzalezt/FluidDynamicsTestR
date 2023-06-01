3535ce9f887d995b575c0afb46f37e75136bdbf4
cgonzalezt <cristipu123@gmail.com>
2023-06-01 09:01:07 -0400

    Closer Cameras set up and player attachment
    Set up the closer cameras by expanding the current code and attaching them to the player. To test later.

--- Assets/CamSwitch.cs
+++ Assets/CamSwitch.cs
@@ -7,6 +7,10 @@ public class CamSwitch : MonoBehaviour
     public GameObject CamXY;
     public GameObject CamYZ;
     public GameObject CamXZ;
+    public GameObject CamXYCU;
+    public GameObject CamYZCU;
+    public GameObject CamXZCU;
+
 
     // Update is called once per frame
     void Update()
@@ -15,16 +19,52 @@ public class CamSwitch : MonoBehaviour
             CamXY.SetActive(true);
             CamYZ.SetActive(false);
             CamXZ.SetActive(false);
+            CamXYCU.SetActive(false);
+            CamYZCU.SetActive(false);
+            CamXZCU.SetActive(false);
         }
         if (Input.GetButtonDown("YZKey")){
             CamXY.SetActive(false);
             CamYZ.SetActive(true);
             CamXZ.SetActive(false);
+            CamXYCU.SetActive(false);
+            CamYZCU.SetActive(false);
+            CamXZCU.SetActive(false);
         }
+
         if (Input.GetButtonDown("XZKey")){
             CamXY.SetActive(false);
             CamYZ.SetActive(false);
             CamXZ.SetActive(true);
+            CamXYCU.SetActive(false);
+            CamYZCU.SetActive(false);
+            CamXZCU.SetActive(false);
+        }
+        
+        if (Input.GetButtonDown("XYCUKey")){
+            CamXY.SetActive(false);
+            CamYZ.SetActive(false);
+            CamXZ.SetActive(false);
+            CamXYCU.SetActive(true);
+            CamYZCU.SetActive(false);
+            CamXZCU.SetActive(false);
+        }
+        if (Input.GetButtonDown("YZCUKey")){
+            CamXY.SetActive(false);
+            CamYZ.SetActive(false);
+            CamXZ.SetActive(false);
+            CamXYCU.SetActive(false);
+            CamYZCU.SetActive(true);
+            CamXZCU.SetActive(false);
+        }
+        
+        if (Input.GetButtonDown("XZCUKey")){
+            CamXY.SetActive(false);
+            CamYZ.SetActive(false);
+            CamXZ.SetActive(false);
+            CamXYCU.SetActive(false);
+            CamYZCU.SetActive(false);
+            CamXZCU.SetActive(true);
         }
     }
 }
